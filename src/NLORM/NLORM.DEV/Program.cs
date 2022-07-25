8b7168e820548b8445d2e41f3a74d54debe6a3a1
Boris <Borismulder@outlook.com>
2022-07-26 00:18:34 +0200

    Update Program.cs

--- src/NLORM/NLORM.DEV/Program.cs
+++ src/NLORM/NLORM.DEV/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("s");
+﻿using NETORM;
+using NETORM.Attributes;
+
+await DatabaseConfig.CreateConnection("");
+
+Console.WriteLine("Done");
+
+[Table("tblprofile")]
+public class Profile { }
+[Table("tblprofile")]
+public class sdfsd { }
+[Table("tblprofile")]
+public class profile { }
+[Table("tblprofile")]
+public class asdfas { }
